5f58454b55c528312e2e4d101db65d6054df91f9
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2021-11-04 10:05:24 -0300

    Criação da função handleMostrarSobre

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,35 @@ import Teste from '../components/Teste';
 import Header from '../components/Header/Header';
 import Sobre from '../components/Sobre/Sobre';
 import Footer from '../components/Footer/Footer';
+import { useState } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 export default function Home() {
+  const [mostrar, setMostrar] = useState(false);
+  const [top, setTop] = useState(false);
+
+  const handleMostrarSobre = () => { 
+    if (!top) {
+      setTop(true);
+      scroll.scrollTo(650); 
+      setMostrar(!mostrar);
+    }
+
+    if (top) {
+      setTop(false);
+      scroll.scrollToTop(); 
+      setTimeout(() => setMostrar(!mostrar), 1000);
+    }
+  }
+
   return (
     <div className="conteiner">
       <Header />
-      <Teste />
-      <Sobre />
+      <Teste 
+        handleMostrarSobre={handleMostrarSobre}
+        mostrar={mostrar}
+      /> 
+      {mostrar && <Sobre />}
       <Footer />
     </div>
   )
