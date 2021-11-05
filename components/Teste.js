5c8654a331c7a0a53a080d06f74ab5aad5951100
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2021-11-05 19:09:50 -0300

    Resolvendo problema na imagem

--- components/Teste.js
+++ components/Teste.js
@@ -6,48 +6,48 @@ import seta1 from '../public/Polygon3.svg';
 import seta2 from '../public/Polygon5.svg';
 import dedimar from '../public/dedimar.png';
 
-function Teste({ handleMostrarSobre, mostrar}) {
-   
+function Teste({ handleMostrarSobre, mostrar }) {
+
     return (
         <div>
             <nav>
                 <ul className="section-list">
-                <li>
-                    <a href="https://github.com/Dedimar-dev">
-                        <Image width={30} height={30} src={ github_light } alt="github" />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/dedimar-santos-3bba211b7/" >
-                        <Image width={30} height={30}src={ linkedin_light } alt="linkedin" />
-                    </a>
-                </li>
-                <li>
-                    <a href="mailto:santos.dedimar@gmail.com?subject=Olá Dedimar," >
-                        <Image width={30} height={30} src={ gmail2 } alt="email" />
-                    </a>
-                </li>
+                    <li>
+                        <a href="https://github.com/Dedimar-dev">
+                            <Image width={30} height={30} src={github_light} alt="github" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/dedimar-santos-3bba211b7/" >
+                            <Image width={30} height={30} src={linkedin_light} alt="linkedin" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:santos.dedimar@gmail.com?subject=Olá Dedimar," >
+                            <Image width={30} height={30} src={gmail2} alt="email" />
+                        </a>
+                    </li>
                 </ul>
             </nav>
-                               
-            <div className="titulo"> 
+
+            <div className="titulo">
                 <div>
                     <h1> Olá, eu sou Dedimar Santos!</h1>
-                    <p>E sou apaixonando por tecnologia.</p>  
+                    <p>E sou apaixonando por tecnologia.</p>
                 </div>
-               
-                <button  
-                    onClick={ handleMostrarSobre } 
+
+                <button
+                    onClick={handleMostrarSobre}
                     className="btns btns_sobre">
-                    {mostrar? 'Menos sobre mim':'Mais sobre mim'}
-                    <Image src={mostrar? seta1: seta2} alt={mostrar? 'Mostrar' : 'Esconder' }/>
+                    {mostrar ? 'Menos sobre mim' : 'Mais sobre mim'}
+                    <Image src={mostrar ? seta1 : seta2} alt={mostrar ? 'Mostrar' : 'Esconder'} />
                 </button>
 
-                
+
             </div>
-           <Image src={dedimar} alt="Dedimar"></Image>
-        </div>  
-        
+            <Image width={300} height={300} src={dedimar} alt="Dedimar"></Image>
+        </div>
+
     )
 }
 
