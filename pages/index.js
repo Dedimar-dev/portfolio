e4b3251309b6f08c16bcd201a932b94e4a31f134
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2021-11-12 12:41:13 -0300

    Algumas mudanças

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
-import Titulo from '../components/Titulo';
-import Header from '../components/Header/Header';
-import Sobre from '../components/Sobre/Sobre';
-import Footer from '../components/Footer/Footer';
 import { useState } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Sobre from '../components/Sobre';
+import Titulo from '../components/Titulo';
 import style from '../styles/Home.module.css';
 
 export default function Home() {
   const [mostrar, setMostrar] = useState(false);
   const [top, setTop] = useState(false);
 
-  const handleMostrarSobre = () => { 
+  const handleMostrarSobre = () => {
     if (!top) {
       setTop(true);
-      scroll.scrollTo(650); 
+      scroll.scrollTo(650);
       setMostrar(!mostrar);
     }
 
     if (top) {
       setTop(false);
-      scroll.scrollToTop(); 
+      scroll.scrollToTop();
       setTimeout(() => setMostrar(!mostrar), 1000);
     }
   }
@@ -30,7 +30,7 @@ export default function Home() {
       <Titulo
         handleMostrarSobre={handleMostrarSobre}
         mostrar={mostrar}
-      /> 
+      />
       {mostrar && <Sobre />}
       <Footer />
     </div>
