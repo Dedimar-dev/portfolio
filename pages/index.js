a4e4e9385be51af994b0d1748ca7c4964f4ffcdf
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-01-20 19:32:53 -0300

    Adicionando mais lógica no componente Home

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,75 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Sobre from '../components/Sobre';
 import Titulo from '../components/Titulo';
+import Habilidades from '../components/Habilidades';
+import Projetos from '../components/Projetos';
 import style from '../styles/Home.module.css';
 
 export default function Home() {
-  const [mostrar, setMostrar] = useState(false);
-  const [top, setTop] = useState(false);
+  const [mostrarSobre, setMostrarSobre] = useState(false);
+  const [mostrarHabilidades, setMostrarHabilidades] = useState(false);
+  const [mostrarProjetos, setMostrarProjetos] = useState(false);
+  const [irParaSobre, setIrParaSobre] = useState(false);
+  const [irParaProjetos, setIrParaProjetos] = useState(false);
+  const [irParaHabilidades, setIrParaHabilidades] = useState(false);
 
   const handleMostrarSobre = () => {
-    if (!top) {
-      setTop(true);
+
+    setMostrarProjetos(false);
+    setIrParaProjetos(false);
+    setMostrarHabilidades(false);
+    setIrParaHabilidades(false);
+
+    if (!irParaSobre) {
+      setIrParaSobre(true);
       scroll.scrollTo(650);
-      setMostrar(!mostrar);
+      setMostrarSobre(!mostrarSobre);
     }
 
-    if (top) {
-      setTop(false);
+    if (irParaSobre) {
+      setIrParaSobre(false);
       scroll.scrollToTop();
-      setTimeout(() => setMostrar(!mostrar), 1000);
+      setTimeout(() => setMostrarSobre(!mostrarSobre), 1000);
+    }
+  }
+
+  const handleMostrarProjetos = () => {
+
+    setMostrarSobre(false);
+    setIrParaSobre(false);
+    setMostrarHabilidades(false);
+    setIrParaHabilidades(false);
+
+    if (!irParaProjetos) {
+      setIrParaProjetos(true);
+      scroll.scrollTo(750);
+      setMostrarProjetos(!mostrarProjetos);
+    }
+
+    if (irParaProjetos) {
+      setIrParaProjetos(false);
+      scroll.scrollToTop();
+      setTimeout(() => setMostrarProjetos(!mostrarProjetos), 1000);
+    }
+  }
+
+  const handleMostrarHabilidades = () => {
+
+    setMostrarProjetos(false);
+    setIrParaProjetos(false);
+    setMostrarSobre(false);
+    setIrParaSobre(false);
+
+    if (!irParaHabilidades) {
+      setIrParaHabilidades(true);
+      scroll.scrollTo(1000);
+      setMostrarHabilidades(!mostrarHabilidades);
+    }
+
+    if (irParaHabilidades) {
+      setIrParaHabilidades(false);
+      scroll.scrollToTop();
+      setTimeout(() => setMostrarHabilidades(!mostrarHabilidades), 1000);
     }
   }
 
@@ -29,9 +81,15 @@ export default function Home() {
       <Header />
       <Titulo
         handleMostrarSobre={handleMostrarSobre}
-        mostrar={mostrar}
+        mostrarSobre={mostrarSobre}
+        handleMostrarProjetos={handleMostrarProjetos}
+        mostrarProjetos={mostrarProjetos}
+        handleMostrarHabilidades={handleMostrarHabilidades}
+        mostrarHabilidades={mostrarHabilidades}
       />
-      {mostrar && <Sobre />}
+      {mostrarSobre && <Sobre />}
+      {mostrarHabilidades && <Habilidades/>}
+      {mostrarProjetos && <Projetos/>}
       <Footer />
     </div>
   )
