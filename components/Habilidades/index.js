634cfb40296dc4f3d796cf639196ee69e0a51c8d
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-03-05 12:42:25 -0300

    Responsividade do componente Habilidades

--- components/Habilidades/index.js
+++ components/Habilidades/index.js
@@ -7,34 +7,35 @@ function Habilidades() {
             Habilidades
          </h1>
          <div className={style.habilidades_image_pai }>
+            <div className={style.habilidades_image_filho} >
                <img 
-                  className={style.habilidades_image_filho} 
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
                />
                <img 
-                  className={style.habilidades_image_filho}  
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-               />
-               <img 
-                  className={style.habilidades_image_filho} 
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-               />
-               <img 
-                  className={style.habilidades_image_filho}  
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
                />
+            </div>
+            <div className={style.habilidades_image_filho} >
                <img 
-                  className={style.habilidades_image_filho}  
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
                />
                <img 
-                  className={style.habilidades_image_filho}  
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
                />
+            </div>
+            <div className={style.habilidades_image_filho} >
                <img 
-                  className={style.habilidades_image_filho}  
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+               />
+               <img 
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+               />
+            </div>
+            <div className={style.habilidades_image_filho} >
+               <img 
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
                />
+            </div> 
          </div>
       </section>
    )
