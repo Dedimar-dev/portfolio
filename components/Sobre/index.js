8ab6aa0940000f79e9ddade54c0047f5a2726e15
Dedimar-dev <santos.dedimar@gmail.com>
2026-01-08 22:51:47 -0300

    Mudança da minha foto e do texto da seção Sobre Mim

--- components/Sobre/index.js
+++ components/Sobre/index.js
@@ -7,22 +7,19 @@ function Sobre() {
         <h1 className={style.titulo_sobre_mim}>
             Sobre mim
         </h1>
-       
         <div className={style.apresentacao}>
             <div className={style.apresentacao_image}>
     
             </div>
             
             <p>
-                Dedi é meu apelido favorito, sou preto, nordestino, moro na zona rural.
-                Acredito que por meio da tecnologia o mundo pode tornar-se mais igualitário.
-                E, minha missão de vida é contribuir para que esse igualitarismo se torne realidade.
-                Tenho como objetivo atuar como Desenvolvedor de software Full Stack. Atualmente, estou em processo de
-                treinamento na,
-                Cubos Academy como bolsista no Programa Cubos Academy e iFood, estou aprendendo HTML, CSS, JavaScript,
-                Lógica, Banco de Dados, SQL,
-                Node.js, React e GIT. Neste momento, busco fazer parte de organizações que tenham como foco, por meio da
-                tecnologia, tornar o mundo um lugar melhor.
+                Dedi é meu apelido favorito, nordestino e morador da zona rural, 
+                movido pela missão de usar a tecnologia para criar um mundo mais 
+                igualitário. Com formação pela Cubos Academy/iFood 
+                (JavaScript, Node.js, PostgreSQL, React, HTML, CSS) 
+                e base acadêmica em Python e C++, tenho grande facilidade em aprender 
+                novas ferramentas e busco evolução constante no setor. Busco atuar em 
+                organizações que, assim como eu, acreditam no impacto social da tecnologia.
             </p>
         </div>
     </section>
