270fe4cc914b59c9ba871f264bfcfcb552da3460
Dedimar-dev <santos.dedimar@gmail.com>
2026-01-15 16:43:54 -0300

    Indentação do Código

--- components/Sobre/index.js
+++ components/Sobre/index.js
@@ -8,21 +8,19 @@ function Sobre() {
                 Sobre mim
             </h1>
             <div className={style.apresentacao}>
-            <div className={style.apresentacao_image}>
-    
+                <div className={style.apresentacao_image}></div>
+                
+                <p>
+                    Dedi é meu apelido favorito, nordestino e morador da zona rural, 
+                    movido pela missão de usar a tecnologia para criar um mundo mais 
+                    igualitário. Com formação pela Cubos Academy/iFood 
+                    (JavaScript, Node.js, PostgreSQL, React, HTML, CSS) 
+                    e base acadêmica em Python e C++, tenho grande facilidade em aprender 
+                    novas ferramentas e busco evolução constante no setor. Busco atuar em 
+                    organizações que, assim como eu, acreditam no impacto social da tecnologia.
+                </p>
             </div>
-            
-            <p>
-                Dedi é meu apelido favorito, nordestino e morador da zona rural, 
-                movido pela missão de usar a tecnologia para criar um mundo mais 
-                igualitário. Com formação pela Cubos Academy/iFood 
-                (JavaScript, Node.js, PostgreSQL, React, HTML, CSS) 
-                e base acadêmica em Python e C++, tenho grande facilidade em aprender 
-                novas ferramentas e busco evolução constante no setor. Busco atuar em 
-                organizações que, assim como eu, acreditam no impacto social da tecnologia.
-            </p>
-        </div>
-    </section>
+        </section>
     )
 }
 
